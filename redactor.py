93b85e0109568cf710b8ea2b0db7c59aa28eb166
amed1x <amedix2@gmail.com>
2023-03-14 12:18:54 +0400

    file too large alert made

---
+++ redactor.py
@@ -0,0 +1,4 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QFont, QPixmap
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton,\
+    QGraphicsDropShadowEffect, QMainWindow, QLabel, QFileDialog
