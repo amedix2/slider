05585d9b0421b835ec77aaa0d4e05e2d7355ab98
amedix <amedix2@gmail.com>
2023-03-20 02:30:40 +0400

    optimized code reading + redesign

--- client.py
+++ client.py
@@ -9,11 +9,10 @@ import sys
 from pathlib import Path
 from threading import Thread
 import keyboard
-from PyQt5 import QtGui
 from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QFont, QPalette, QColor
+from PyQt5.QtGui import QFont
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QGraphicsDropShadowEffect, QMainWindow, QLabel, \
-    QFileDialog, QMessageBox, QScrollBar, QVBoxLayout, QScrollArea, QLineEdit, QTextEdit, QGridLayout
+    QFileDialog, QMessageBox, QLineEdit
 
 import config
 
@@ -66,7 +65,7 @@ def conn_to_serv(selfobj):
     try:
         sock.connect((config.app_settings.ip_serv, 11111))
         key = str(sock.recv(16))[2:-1]
-        connection.set_room(selfobj, 'sending file...')
+        connection.set_room(selfobj, 'load')
         send_file(sock, key)
         room_id = str(sock.recv(4))[2:-1]
         print(room_id)
@@ -99,49 +98,53 @@ def redactor_launch():
 class main_window(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.instruction = instruction(self)
+        self.settings = settings(self)
+        self.connection = connection(self)
+        self.win_file = file(self)
+        self.btn_set = QPushButton('Настройки', self)
+        self.btn_q = QPushButton('QR-код', self)
+        self.btn_i = QPushButton('Инструкция', self)
+        self.btn_f = QPushButton('Выбрать файл', self)
+        self.btn_c = QPushButton('Подключить\nустройство', self)
+        self.color_btn = config.colors.color_supp
         self.initUI()
 
     def initUI(self):
-        self.color_btn = config.colors.color_supp
 
         self.setGeometry(560, 240, 800, 600)
         self.setWindowTitle(config.app_settings.version)
         self.setStyleSheet(f'background-color: {config.colors.color_main}; border-radius: 15')
 
-        self.btn_c = QPushButton('Подключить\nустройство', self)
         self.btn_c.setFont(QFont("Times", 65, QFont.Bold))
         self.btn_c.resize(760, 300)
         self.btn_c.move(20, 20)
         self.btn_c.setStyleSheet(f'background-color: {self.color_btn}; border-radius: 15')
-        self.btn_c.setGraphicsEffect(QGraphicsDropShadowEffect(blurRadius=15, xOffset=7, yOffset=7))
+        self.btn_c.setGraphicsEffect(QGraphicsDropShadowEffect(blurRadius=3, xOffset=4, yOffset=4))
 
-        self.btn_f = QPushButton('Выбрать файл', self)
         self.btn_f.setFont(QFont("Times", 23, QFont.Bold))
         self.btn_f.resize(370, 110)
         self.btn_f.move(20, 340)
         self.btn_f.setStyleSheet(f'background-color: {self.color_btn}; border-radius: 15')
-        self.btn_f.setGraphicsEffect(QGraphicsDropShadowEffect(blurRadius=15, xOffset=7, yOffset=7))
+        self.btn_f.setGraphicsEffect(QGraphicsDropShadowEffect(blurRadius=3, xOffset=4, yOffset=4))
 
-        self.btn_i = QPushButton('Инструкция', self)
         self.btn_i.setFont(QFont("Times", 23, QFont.Bold))
         self.btn_i.resize(370, 110)
         self.btn_i.move(410, 340)
         self.btn_i.setStyleSheet(f'background-color: {self.color_btn}; border-radius: 15')
-        self.btn_i.setGraphicsEffect(QGraphicsDropShadowEffect(blurRadius=15, xOffset=7, yOffset=7))
+        self.btn_i.setGraphicsEffect(QGraphicsDropShadowEffect(blurRadius=3, xOffset=4, yOffset=4))
 
-        self.btn_q = QPushButton('QR-код', self)
         self.btn_q.setFont(QFont("Times", 23, QFont.Bold))
         self.btn_q.resize(370, 110)
         self.btn_q.move(20, 470)
         self.btn_q.setStyleSheet(f'background-color: {self.color_btn}; border-radius: 15')
-        self.btn_q.setGraphicsEffect(QGraphicsDropShadowEffect(blurRadius=15, xOffset=7, yOffset=7))
+        self.btn_q.setGraphicsEffect(QGraphicsDropShadowEffect(blurRadius=3, xOffset=4, yOffset=4))
 
-        self.btn_set = QPushButton('Настройки', self)
         self.btn_set.setFont(QFont("Times", 23, QFont.Bold))
         self.btn_set.resize(370, 110)
         self.btn_set.move(410, 470)
         self.btn_set.setStyleSheet(f'background-color: {self.color_btn}; border-radius: 15')
-        self.btn_set.setGraphicsEffect(QGraphicsDropShadowEffect(blurRadius=15, xOffset=7, yOffset=7))
+        self.btn_set.setGraphicsEffect(QGraphicsDropShadowEffect(blurRadius=3, xOffset=4, yOffset=4))
 
         self.btn_c.clicked.connect(self.con)
         self.btn_f.clicked.connect(self.file)
@@ -150,7 +153,6 @@ class main_window(QMainWindow):
         self.btn_i.clicked.connect(self.ins)
 
     def file(self):
-        self.win_file = file(self)
         self.win_file.show()
 
     def guide(self):
@@ -160,18 +162,15 @@ class main_window(QMainWindow):
         global opened_con
         if not opened_con:
             opened_con = True
-            self.connection = connection(self)
             self.connection.show()
 
     def qr(self):
         os.system('start qr-code.jpg')
 
     def set(self):
-        self.settings = settings(self)
         self.settings.show()
 
     def ins(self):
-        self.instruction = instruction(self)
         self.instruction.show()
 
     def closeEvent(self, event):
@@ -189,6 +188,10 @@ class main_window(QMainWindow):
 class file(QWidget):
     def __init__(self, *a):
         super().__init__()
+        self.path = ''
+        self.btn_uploadFile = QPushButton('Загрузить свой файл', self)
+        self.btn_myProjects = QPushButton('Мои проекты', self)
+        self.btn_openRedactor = QPushButton('Открыть редактор', self)
         self.initUI()
 
     def initUI(self):
@@ -196,26 +199,23 @@ class file(QWidget):
         self.setWindowTitle('Add file')
         self.setStyleSheet(f'background-color: {config.colors.super_light_grey};')
 
-        self.btn_openRedactor = QPushButton('открыть редактор', self)
         self.btn_openRedactor.setFont(QFont("Times", 23, QFont.Bold))
         self.btn_openRedactor.resize(480, 100)
         self.btn_openRedactor.move(20, 20)
         self.btn_openRedactor.setStyleSheet(f'background-color: {config.colors.light_grey}; border-radius: 15')
-        self.btn_openRedactor.setGraphicsEffect(QGraphicsDropShadowEffect(blurRadius=15, xOffset=7, yOffset=7))
+        self.btn_openRedactor.setGraphicsEffect(QGraphicsDropShadowEffect(blurRadius=3, xOffset=4, yOffset=4))
 
-        self.btn_myProjects = QPushButton('мои проекты', self)
         self.btn_myProjects.setFont(QFont("Times", 23, QFont.Bold))
         self.btn_myProjects.resize(480, 100)
         self.btn_myProjects.move(20, 140)
         self.btn_myProjects.setStyleSheet(f'background-color: {config.colors.light_grey}; border-radius: 15')
-        self.btn_myProjects.setGraphicsEffect(QGraphicsDropShadowEffect(blurRadius=15, xOffset=7, yOffset=7))
+        self.btn_myProjects.setGraphicsEffect(QGraphicsDropShadowEffect(blurRadius=3, xOffset=4, yOffset=4))
 
-        self.btn_uploadFile = QPushButton('загрузить свой файл', self)
         self.btn_uploadFile.setFont(QFont("Times", 23, QFont.Bold))
         self.btn_uploadFile.resize(480, 100)
         self.btn_uploadFile.move(20, 260)
         self.btn_uploadFile.setStyleSheet(f'background-color: {config.colors.light_grey}; border-radius: 15')
-        self.btn_uploadFile.setGraphicsEffect(QGraphicsDropShadowEffect(blurRadius=15, xOffset=7, yOffset=7))
+        self.btn_uploadFile.setGraphicsEffect(QGraphicsDropShadowEffect(blurRadius=3, xOffset=4, yOffset=4))
 
         self.btn_uploadFile.clicked.connect(self.other_docs)
         self.btn_myProjects.clicked.connect(self.my_docs)
@@ -249,6 +249,9 @@ class file(QWidget):
 class connection(QWidget):
     def __init__(self, *a):
         super().__init__()
+        self.ab = QLabel(self)
+        self.us = QLabel(self)
+        self.rl = QLabel(self)
         self.initUI()
 
     def initUI(self):
@@ -260,21 +263,18 @@ class connection(QWidget):
         self.setWindowTitle('Connection')
         self.setStyleSheet(f'background-color: {config.colors.light_grey};')
 
-        self.rl = QLabel(self)
         self.rl.setFont(QFont("Times", 70, QFont.Bold))
         self.rl.setText('connecting...')
         self.rl.resize(800, 300)
         self.rl.move(0, 20)
         self.rl.setAlignment(Qt.AlignCenter)
 
-        self.us = QLabel(self)
         self.us.setFont(QFont("Times", 33, QFont.Bold))
         self.us.setText(f'Пользователь не подключен')
         self.us.resize(800, 100)
         self.us.move(0, 300)
         self.us.setAlignment(Qt.AlignCenter)
 
-        self.ab = QLabel(self)
         self.ab.setFont(QFont("Times", 15, QFont.Cursive))
         self.ab.setText(
             'Отсканируйсте QR-код с помощью камеры на вашем смартфоне\n или самостоятельно найдите @remoteamedixbot в '
@@ -316,6 +316,10 @@ class connection(QWidget):
 class settings(QWidget):
     def __init__(self, *a):
         super().__init__()
+        self.switchColor = QPushButton("Тёмная тема", self)
+        self.saveIp = QPushButton('Сохранить', self)
+        self.ip = QLineEdit(self)
+        self.us = QLabel(self)
         self.initUI()
 
     def initUI(self):
@@ -323,36 +327,33 @@ class settings(QWidget):
         self.setWindowTitle('Feedback')
         self.setStyleSheet(f'background-color: {config.colors.super_light_grey};')
 
-        self.us = QLabel(self)
         self.us.setFont(QFont("Times", 27, QFont.Bold))
         self.us.setText(f'Обратная связь:\namedix2@gmail.com\nt.me/amedix2\nvk.com/amedix')
         self.us.resize(520, 200)
         self.us.move(0, 180)
         self.us.setAlignment(Qt.AlignCenter)
 
-        self.ip = QLineEdit(self)
         self.ip.move(20, 30)
         self.ip.resize(260, 50)
         self.ip.setStyleSheet(f'background-color: {config.colors.light_grey}; border-radius: 15')
-        self.ip.setGraphicsEffect(QGraphicsDropShadowEffect(blurRadius=15, xOffset=7, yOffset=7))
+        self.ip.setGraphicsEffect(QGraphicsDropShadowEffect(blurRadius=3, xOffset=4, yOffset=4))
         self.ip.setPlaceholderText("Введите ip")
         self.ip.setFont(QFont("Times", 20, QFont.Bold))
         self.ip.setAlignment(Qt.AlignCenter)
 
-        self.saveIp = QPushButton('Сохранить', self)
         self.saveIp.setFont(QFont("Times", 23, QFont.Bold))
         self.saveIp.resize(200, 50)
         self.saveIp.move(300, 30)
         self.saveIp.setStyleSheet(f'background-color: {config.colors.light_grey}; border-radius: 15')
-        self.saveIp.setGraphicsEffect(QGraphicsDropShadowEffect(blurRadius=15, xOffset=7, yOffset=7))
+        self.saveIp.setGraphicsEffect(QGraphicsDropShadowEffect(blurRadius=3, xOffset=4, yOffset=4))
 
-        self.switchColor = QPushButton("Тёмная тема", self)
         self.switchColor.setFont(QFont("Times", 21, QFont.Bold))
         self.switchColor.resize(200, 50)
         self.switchColor.move(40, 110)
         self.switchColor.setCheckable(True)
         self.switchColor.setStyleSheet(f'background-color: {config.colors.light_grey}; border-radius: 15')
-        self.switchColor.setGraphicsEffect(QGraphicsDropShadowEffect(blurRadius=15, xOffset=7, yOffset=7))
+        self.switchColor.setGraphicsEffect(QGraphicsDropShadowEffect(blurRadius=3, xOffset=4, yOffset=4))
+
 
 '''
         self.saveIp.clicked.connect(self.SaveIp)
@@ -369,6 +370,7 @@ class settings(QWidget):
     def SaveIp(self):
         config.app_settings.set_ip(self.ip.text())
 '''
+
 
 class instruction(QWidget):
     def __init__(self, *a):
